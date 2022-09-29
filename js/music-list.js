2dd639da86b825c706836e3f4113142ecf0bcf87
chorong Jo <whchfhd53@gmail.com>
2022-09-29 13:07:10 +0900

    Create music-list.js

---
+++ js/music-list.js
@@ -0,0 +1,33 @@
+let allMusic = [
+    {
+        name: "지코 - 너는 나 나는 너",
+        artist:"지코",
+        img:"image2",
+        src:"song2"
+    },
+    {
+        name: "에디킴 - 내 입술 따뜻한 커피처럼",
+        artist:"에디킴",
+        img:"image1",
+        src:"song1"
+    },
+    {
+        name: "에릭남 - 봄인가 봐",
+        artist:"에릭남",
+        img:"image3",
+        src:"song3"
+    },
+    {
+        name: "10cm - 비밀연애",
+        artist:"10cm",
+        img:"image4",
+        src:"song4"
+    },
+    {
+        name: "윤딴딴 - 27살의 고백",
+        artist:"윤딴딴",
+        img:"image5",
+        src:"song5"
+    }
+
+]
